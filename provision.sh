650334a96bf8416c8d25690902fa6e744bcdd229
Scott T. Hardin <scott@hnsc.de>
2015-10-08 10:02:00 +0200

    initial commit

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Provision Squid Debian Proxy
+
+set -x
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y squid-deb-proxy
+
+/etc/init.d/squid-deb-proxy stop
+(cd / && patch -p1 < /vagrant/cache/squid-deb-proxy.patch)
+chown -R vagrant.vagrant /var/log/squid-deb-proxy
+/etc/init.d/squid-deb-proxy start
