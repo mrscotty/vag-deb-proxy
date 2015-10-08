0fe96df749cb6712c6d93f0ff93882f1287a4f1f
Scott T. Hardin <scott@hnsc.de>
2015-10-08 10:32:58 +0200

    fix location of patchfile in provision.sh

--- provision.sh
+++ provision.sh
@@ -9,6 +9,6 @@ apt-get update
 apt-get install -y squid-deb-proxy
 
 /etc/init.d/squid-deb-proxy stop
-(cd / && patch -p1 < /vagrant/cache/squid-deb-proxy.patch)
+(cd / && patch -p1 < /vagrant/squid-deb-proxy.patch)
 chown -R vagrant.vagrant /var/log/squid-deb-proxy
 /etc/init.d/squid-deb-proxy start
